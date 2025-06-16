d5f2a722da58f5b5d5f807105848167bbf2c0e7b
Alex Domina <adomina@alexs-mbp.mynetworksettings.com>
2025-06-16 12:28:52 -0400

    Space the lightbox button

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -7,7 +7,7 @@ function Lightbox({ image, onClose }: {image: Image, onClose: () => void}) {
         <div className='lightbox' id='lightbox'>
             <div className='lightbox-content' id='lightbox-content'>
                 <img src={image.image} alt={image.alt} id='image-enlarged' key='image_enlarged'/>
-                <span className='button' id='close-button-span'>
+                <span className='button' id='close-button-span' style={{ marginTop: '5px' }}>
                     <button onClick={onClose} aria-label="Close Lightbox" id='close-button'>Close</button>
                 </span>
             </div>
